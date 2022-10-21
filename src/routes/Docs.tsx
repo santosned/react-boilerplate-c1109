a0b73be787d44da80f89c69868c9d34d642591dd
E. Santos <99996904+santosned@users.noreply.github.com>
2022-10-21 20:22:33 -0300

    Add react-query hook to fetch data

--- src/routes/Docs.tsx
+++ src/routes/Docs.tsx
@@ -1,33 +1,34 @@
 import React from 'react'
-import { APP_PAGE_NAME } from '.'
+import { useQuery } from '@tanstack/react-query'
 import './Docs.css'
-
-type DocsData = Array<{ link: string; text: string }>
+import { APP_PAGE_NAME } from '.'
+import ReactLogo from '~/components/ReactLogo'
 
 const Docs: React.FC = () => {
-  const [docs, setDocs] = React.useState<DocsData>([])
+  const { status, data, error } = useQuery(
+    ['docs'],
+    async () => await fetch('/docs.json').then(async res => await res.json())
+  )
 
   React.useEffect(() => {
     document.title = `Documentation - ${APP_PAGE_NAME}`
   }, [])
 
-  React.useEffect(() => {
-    if (docs.length === 0) {
-      fetch('/docs.json')
-        .then(async res => await res.json())
-        .then(data => setDocs(data.docs))
-        .catch(err => {
-          console.log(err)
-        })
-    }
-  }, [])
+  if (status === 'loading') {
+    return <ReactLogo />
+  }
+
+  if (status === 'error') {
+    console.log(error)
+    return <></>
+  }
 
   return (
     <>
       <h1>Documentation</h1>
       <p>For more information see the documentation of each project</p>
       <ul className="list">
-        {docs.map(doc => (
+        {data.docs.map((doc: { text: any; link: string | undefined }) => (
           <li key={doc.text}>
             <a href={doc.link} target="_blank" rel="noopener noreferrer">
               {doc.text}
